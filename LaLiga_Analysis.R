3126fe0cc9e83899ab27130ca3dcbeadf3c10de8
Alejandro Cepeda <acepeda19@usf.edu>
2022-03-16 19:04:11 -0400

    Update LaLiga_Analysis.R

--- LaLiga_Analysis.R
+++ LaLiga_Analysis.R
@@ -7,3 +7,10 @@
 library(worldfootballR)
 library(tidyverse)
 library(ggrepel)
+
+# loading LaLiga 21/22 FBref data
+laLiga.URL <- fb_teams_urls("https://fbref.com/en/comps/12/La-Liga-Stats")
+laLiga.teams <- fb_team_player_stats(team_urls=laLiga.URL, stat_type="standard")
+
+# plot goal vs xg performance
+#laLiga.teams %>%
