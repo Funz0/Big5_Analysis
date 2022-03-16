a0098d09435a78765e36835625776cc92056a9ca
Alejandro Cepeda <acepeda19@usf.edu>
2022-03-16 19:03:58 -0400

    Add LaLiga_Analysis.R

---
+++ LaLiga_Analysis.R
@@ -0,0 +1,9 @@
+### DUMMY SCRIPT FOR DRAFTS/STORED FUNCTIONS ###
+
+# libraries
+#install.packages("worldfootballR")
+#install.packages("tidyverse")
+#devtools::install_github("JaseZiv/worldfootballR", ref = "main")
+library(worldfootballR)
+library(tidyverse)
+library(ggrepel)
